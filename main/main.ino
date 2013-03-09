c19b34b95e6101876a510ff983c6639cb2e5505d
Cary <melstrom@gmail.com>
2013-03-09 15:00:29 -0800

    Initial algorithm to code

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,82 @@
-int receiver[n];
-int infrared[n];
-int currentDirection;
-void setup() {
-  // put your setup code here, to run once:
+#include <motor.h>
+#include <sensor.h>
+#include <sheep_interrupts.h>
 
+int distance[4];
+int beacon[4];
+
+int posx;
+int posy;
+
+int homex;
+int homey;
+
+int sheep;
+
+/*
+Forward facing direction of the robot
+1 = North
+2 = East
+3 = South
+4 = West
+*/
+int currentDirection;
+
+void setup() {
+  //We set the starting direction of the robot to 'North'; every change after this point will be the relative direction
+  currentDirection = 1;
+  
+  //Variable declaration(s)
+  sensor drive;
+  sheep = 0;
+  
+  findInitialPosition();
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  readingUpdate();
-  int direction = determinDirection();
-  int steps = determineSteps(direction, ??)
+  randomWalk();
+  goHome();
+}
+
+void randomWalk() {
+  while (y < 120) {
+    if (isRescueComplete) {
+      return;
+    }
+    moveForward();
+    thicketSearch();
+  }
+  if (x < 80) {
+    changeOrientation(2);
+    while (x <120)
+    {
+      if (isRescueComplete) {
+        return;
+      }
+      moveForward();
+      thicketSearch();
+    }
+  else {
+    changeOrientation(4);
+    while (x > 40) {
+      if (isRescueComplete) {
+        return;
+      }
+      moveForward();
+      thicketSearch();
+  }
+  while (y > 40) {
+    if (isRescueComplete) {
+      return;
+    }
+    moveForward();
+    thicketSearch();
+  }
 }
 
 void updateReadings() {
- for (all beaconReceivers) {
+ for (all receivres) {
  receiver[n] <- receiver.driver.distance
  } 
  
@@ -23,42 +85,182 @@ void updateReadings() {
  }
 }
 
-int determineDirection() {
-  int maximum1 = 0;
-  int maximum2 = 0;
- for (all beaconReceivers) {
-   if (receiver[n] > maximum1) { maximum1 = receiver[n]; }
-   else if (receiver[n] > maximum2) {maximum2 = receiver[n]; }
- }
+//Detects the presence of a beacon
+int detectBeacon() {
+  for (int i = 0; i <4; i++) {
+    if (receiver[i] > 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
-int determineSteps(int direction, int distance) {
-  if (infrared[direction] < distance) {
-    return infrared[direction];
+//If a beacon is present, search for the thicket
+void thicketSearch() {
+  int targetDirection = detectBeacon();
+  if (targetDirection != 0) {
+    while (sheep == 0) {
+      changeOrientation(targetDirection);
+      moveForward();
+    }
+  }
+}
+
+//Move functions with coordinates
+void turnLeft ()
+{
+  if (currentDirection == 1) {
+    currentDirection = 4;
   }
   
-  else return distance;
+  else {
+    currentDirection --;
+  }
+  drive.rotate90Left();
 }
 
-void move (int direction, int distance) {
-  int difference = currentDirection - direction;
-  switch difference {
-    case 0:
-    break;
-    case 1:
-    turnLeft;
-    break;
-    case 2:
-    turnLeft; turnLeft;
-    break;
-    case 3;
-    turnLeft; turnLeft; turnLeft;
-    break;
-    case 4;
-    turnRight;
-    break;
+//Move functions with coordinates
+void turnRight ()
+{
+  if (currentDirection == 4) {
+    currentDirection = 1;
   }
-  for (distance) {
-    moveForward;
+  
+  else {
+    currentDirection ++;
+  }
+  drive.rotate90Right();
+}
+
+void moveForward () {
+  switch (currentDirection) {
+    case 1:
+      posy++;
+      break;
+    case 2:
+      posx++;
+      break;
+    case 3:
+      posy--;
+      break;
+    case 4:
+      posx--;
+      break;
+    default:
+      break;
+  }
+  drive.goStraightTile(1);
+}
+
+void uTurn () {
+  switch (currentDirection)
+  {
+    case 1:
+      currentDirection = 3;
+      break;
+    case 2:
+      currentDirection = 4;
+      break;
+    case 3:
+      currentDirection = 1;
+      break;
+    case 4:
+      currentDirection = 2;
+      break;
+    default:
+      break;
+  }
+  drive.rotate180Right();
+}
+
+void changeOrientation (int targetDirection) {
+  if (currentDirection == targetDirection) {
+    return;
+  }
+  switch (targetDirection) {
+    case 1:
+      switch (currentDirection) {
+        case 2:
+          turnLeft();
+          break;
+        case 3:
+          uTurn();
+          break;
+        case 4();
+          turnRight();
+          break;
+        default:
+          break;
+      }
+    case 2:
+      switch (currentDirection) {
+        case 3:
+          turnLeft();
+          break;
+        case 4:
+          uTurn();
+          break;
+        case 1();
+          turnRight();
+          break;
+        default:
+          break;
+      }
+    case 3:
+      switch (currentDirection) {
+        case 4:
+          turnLeft();
+          break;
+        case 1:
+          uTurn();
+          break;
+        case 2();
+          turnRight();
+          break;
+        default:
+          break;
+      }
+    case 4:
+      switch (currentDirection) {
+        case 1:
+          turnLeft();
+          break;
+        case 2:
+          uTurn();
+          break;
+        case 3();
+          turnRight();
+          break;
+        default:
+          break;
+      }
+  }
+}
+
+bool isRescueComplete() {
+  if (sheep == 2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void findInitialPosition() {
+  //Find initial coordinates
+  if (left and right sensors read nothing,) {
+    turnLeft();
+    offset = 0;
+    while (front reads nothing,) {
+      moveForward();
+      offset++;
+    }
+    posx = front reading;
+    posy = 1;
+    
+    homex = front reading + offset;
+    homey = 1;
+    
+    turnRight();
   }
 }
